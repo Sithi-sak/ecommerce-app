f51f3d4c0b66bc7cfe5e7679be19ba43514a654f
Leak Sithisak <sithisakleak001@gmail.com>
2025-03-04 20:58:18 +0700

    java version changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,9 +26,10 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+    buildToolsVersion = "35.0.0"
 }
 
 dependencies {
